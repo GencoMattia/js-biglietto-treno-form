43de6d970171cf2cd50188b7c14df5e8ae448bfb
GencoMattia <gencomattia@hotmail.it>
2024-05-09 16:45:13 +0200

    succesfully implemented function to calculate ticket price and log to console

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ let userAge;
 
 
 // va applicato uno sconto del 24.552% per gli under 21
-const minorAge = 21
-const minorDiscount = 24.552
+const minorAge = 21;
+const minorDiscount = 24.552;
 
 // va applicato uno sconto del 37.893% per gli over 63.
-const elderAge = 63
-const elderDiscount = 37.893
+const elderAge = 63;
+const elderDiscount = 37.893;
 
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
@@ -23,7 +23,30 @@ const elderDiscount = 37.893
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 
+const pricePerKm = 0.267113;
+let fullPrice;
 
-submitButton = document.querySelector ("div.form>button")
+submitButton = document.querySelector ("div.form>button");
 
-console.log (submitButton)
+console.log (submitButton);
+
+submitButton.addEventListener("click", function(){
+    travelDistance = Number.parseFloat(document.querySelector ("input#travel-distance").value);
+    userAge = Number.parseInt(document.querySelector ("input#user-age").value);
+
+    fullPrice = pricePerKm * travelDistance;
+    let discount;
+    
+    if (userAge < minorAge) {
+        discount = (fullPrice * minorDiscount) / 100;
+    } else if (userAge > elderAge) {
+        discount = (fullPrice * elderDiscount) / 100;
+    } else {
+        discount = 0;
+    }
+
+    ticketPrice = fullPrice - discount
+    
+    console.log (travelDistance, userAge);
+    console.log (ticketPrice)
+})
